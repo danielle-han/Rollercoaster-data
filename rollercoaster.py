0601ea59b577419761ec8ab7beb1ba2776bd5433
danielle-han <61366948+danielle-han@users.noreply.github.com>
2022-12-14 23:15:35 -0800

    Created project
    project initialized and necessary libraries/packages downloaded

---
+++ rollercoaster.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas
+
+# send HTTP get request for rollercoaster website
+## response = response.get()
+
+# store content of response in var
+# parse content with beautifulsoup
+
